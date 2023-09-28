bd22c11dc8b20454928d9c31a06b13b48ca3e745
Jessica Dinova <dino0001@hz.nl>
2023-09-28 15:02:05 +0200

    responsive text and field next to the image

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -11,7 +11,6 @@
 
 <!-- Header -->
 <header id="header" class="alt">
-   <!-- <a class="logo"><img class="w-1/2 is-align-self-center" src="images/paper-boat.jpg"></a>-->
     <a href="/" class="logo"><img class="scale-50" src="images/paper-boat.jpg"></a>
     <a href="{{ route('login') }}">Login</a>
     <a href="#menu">Menu</a>
