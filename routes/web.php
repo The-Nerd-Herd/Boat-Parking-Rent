d7143db3604de7d721ef4100d49d73089813d7f2
Simon Manassé <simo0047@hz.nl>
2023-09-28 17:03:58 +0200

    changed input border to black

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', function () {
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
