69aeb0aab00660bc40d4367f5ed73f8fedd9109c
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-03-08 14:42:33 +0100

    added website link

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -3,10 +3,13 @@
 <section class="w-full h-full px-6 py-8 mx-auto bg-white dark:bg-gray-900">
     <main class="w-full h-full flex flex-col justify-center items-center">
         <div class="flex w-90% lg:w-1/2 bg-gray-100 flex-col items-center rounded-lg shadow-2xl lg:p-0">
-            <header class="w-full flex flex-row items-center bg-black h-15">
+            <header class="w-full flex flex-row justify-between items-center bg-black h-15">
                 <div class="h-full">
                     <img src="/images/paper-boat.jpg" class="h-full z-99 scale-50" alt="boat">
                 </div>
+                <a class="text-gray-300 justify-between mr-2" href="https://ligplaatsenzeeland.nl/">
+                    ligplaatsenzeeland.nl
+                </a>
             </header>
             <div class="p-4">
                 <h1 class="text-3xl lg:text-5xl mt-2">Buyer found !</h1>
